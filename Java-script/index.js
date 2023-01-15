3a78be5b2f39d083db0e9895e2ae5ad6eb7fd5fb
Mhatons <mhatons@gmail.com>
2023-01-15 17:52:13 +0100

    added css and javascript

---
+++ Java-script/index.js
@@ -0,0 +1,43 @@
+let name = document.getElementById("name")
+let phone = document.getElementById("phone")
+let email = document.getElementById("email")
+let subject = document.getElementById("subject")
+let comment = document.getElementById("comment")
+let btn = document.getElementById("btn")
+
+//  const myValues = {(
+//     name.value, 
+//     phone.value,
+//     email.value,
+//      subject.value,
+//      comment.value
+//  )}
+
+
+// function validate(){
+//     if(name.value || phone.value || email.value || subject.value || comment.value !== ""){
+        
+//         fetch("http://localhost:4001/post_form", {
+//             method: "POST",
+//             headers: { "Content-Type": "application/json" },
+//             body: JSON.stringify({
+//                 name: name.value,
+//                 email: email.value,
+//                 phone: phone.value,
+//                 subject: subject.value,
+//                 comment: comment.value
+//             })
+//         }).then((resp) => resp.json())
+//         .then((data) => {
+//             console.log(data)
+//         })
+        
+//     }
+//     else{
+//         console.log("fields are empty")
+//     }
+// }
+
+// btn.onclick() = function(){
+//     validate()
+// }
